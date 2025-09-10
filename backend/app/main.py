b98a0575d38b9a0fe0e9bb388b1959660ffc0796
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2025-09-10 12:37:34 -0500

    Add user routes to app

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
-from app.api.routes import auth_routes
+from app.api.routes import auth_routes, user_routes
 from app.core.config import settings
 
 app = FastAPI(title=settings.PROJECT_NAME)
 
 app.include_router(auth_routes.router)
+app.include_router(user_routes.router)
