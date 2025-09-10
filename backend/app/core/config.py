e4efa00b4deefdeef0aed8e402391c4d416ec212
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2025-09-10 09:55:52 -0500

    Add config from env variables

---
+++ backend/app/core/config.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
+    PROJECT_NAME: str = "Promptly API"
+    MONGO_URI: str = "mongodb://localhost:27017"
+    MONGO_DB: str = "promptly"
+
+    JWT_SECRET: str = "supersecret" 
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+
+settings = Settings()
+
+if __name__ == "__main__":
+    print(settings.model_dump())
