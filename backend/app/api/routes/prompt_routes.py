9df7d334930cb20d955845113d4a8abcf1e395ab
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2025-09-15 22:57:59 -0500

    add endpoint to delete prompts, update response status

--- backend/app/api/routes/prompt_routes.py
+++ backend/app/api/routes/prompt_routes.py
@@ -1,17 +1,17 @@
 from typing import List
-from datetime import datetime
 
 from fastapi import APIRouter, status, HTTPException
-from bson.objectid import ObjectId
 
 from app.api.dependencies import RepositoriesDependency, UserDependency
 from app.schemas.prompt_schema import Prompt, PromptCreate, PromptUpdate
 
 router = APIRouter(prefix="/prompts", tags=["Prompts"])
 
+
 @router.get("/", response_model=List[Prompt], status_code=status.HTTP_200_OK)
 async def get_prompts(repos: RepositoriesDependency):
     return await repos.prompts.get();
+
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_prompt(prompt: PromptCreate, user: UserDependency, repos: RepositoriesDependency):
@@ -27,7 +27,8 @@ async def create_prompt(prompt: PromptCreate, user: UserDependency, repos: Repos
 
     return { "message": "New prompt created"}
 
-@router.patch("/{prompt_id}")
+
+@router.patch("/{prompt_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def update_prompt(
     prompt_id: str,
     prompt_update: PromptUpdate,
@@ -35,11 +36,19 @@ async def update_prompt(
     repos: RepositoriesDependency
     ):
     prompt = await repos.prompts.get_by_id(prompt_id)
-    prompt_user_id = str(prompt.get("user_id"))
 
-    if (user.id != prompt_user_id):
+    if user.id != prompt.user_id:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="No authorized to update")
     
     update_data = prompt_update.model_dump(exclude_unset=True)
     await repos.prompts.update(prompt_id, update_data)
-    return { "Message": "Updated" }
+
+
+@router.delete("/{prompt_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_prompt(prompt_id: str, user: UserDependency, repos: RepositoriesDependency):
+    prompt = await repos.prompts.get_by_id(prompt_id)
+
+    if user.id != prompt.user_id:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="No authorized to delete")
+    
+    await repos.prompts.delete(prompt_id)
