fa40571c150f80d30b52b23ecbe12ac8fc2b2420
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2025-09-16 10:52:08 -0500

    Add endpoint to get user's prompts

--- backend/app/api/routes/user_routes.py
+++ backend/app/api/routes/user_routes.py
@@ -1,12 +1,25 @@
 from fastapi import APIRouter
-from app.api.dependencies import UserDependency
+from bson import ObjectId
+
+from app.api.dependencies import UserDependency, RepositoriesDependency
+from app.schemas.prompt_schema import Prompt
+from app.schemas.user_schema import User
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
 
-@router.get("/me")
+@router.get("/me", response_model=User)
 async def get_me(current_user: UserDependency):
     """
     Get current logged in user
     """
-    return {"user": current_user}
+    return current_user
+
+
+@router.get("/me/prompts", response_model=list[Prompt], summary="Get my prompts")
+async def get_my_prompts(current_user: UserDependency, repos: RepositoriesDependency):
+    """
+    Get prompts created by the current user
+    """
+    return await repos.prompts.get({"user_id": ObjectId(current_user.id)})
+
