043f8af98e2f8852d949d2e0de28612dfa0c50ba
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2025-09-15 22:57:34 -0500

    add method to delete prompts

--- backend/app/repositories/prompts_repository.py
+++ backend/app/repositories/prompts_repository.py
@@ -23,7 +23,8 @@ class PromptsRepository:
 
     async def get_by_id(self, id: str) -> Prompt:
         object_id = ObjectId(id)
-        return await self.collection.find_one({ "_id": object_id })
+        document = await self.collection.find_one({ "_id": object_id })
+        return document_to_prompt(document)
     
     async def create(self, prompt_data: dict, user_id: str):
         prompt_data.update({
@@ -33,6 +34,9 @@ class PromptsRepository:
         result = await self.collection.insert_one(prompt_data)
         return str(result.inserted_id)
 
-    async def update(self, id: str, update_data: dict):
-        return await self.collection.update_one({ "_id": ObjectId(id) }, { "$set": update_data  })
+    async def update(self, prompt_id: str, update_data: dict):
+        return await self.collection.update_one({ "_id": ObjectId(prompt_id) }, { "$set": update_data  })
+    
+    async def delete(self, prompt_id: str):
+        return await self.collection.delete_one({ "_id": ObjectId(prompt_id) })
  
