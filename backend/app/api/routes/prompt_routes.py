a0d3196b6b435a97414936133a8d8afff4f11ec1
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2025-09-16 12:18:36 -0500

    Add endpoints to get, create and delete comments

--- backend/app/api/routes/prompt_routes.py
+++ backend/app/api/routes/prompt_routes.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, status, HTTPException
 
 from app.api.dependencies import RepositoriesDependency, UserDependency
 from app.schemas.prompt_schema import Prompt, PromptCreate, PromptUpdate
+from app.schemas.comment_schema import Comment, CommentCreate
 
 router = APIRouter(prefix="/prompts", tags=["Prompts"])
 
@@ -62,3 +63,36 @@ async def delete_prompt(prompt_id: str, user: UserDependency, repos: Repositorie
     await repos.prompts.delete(prompt_id)
 
 
+@router.get("/{prompt_id}/comments", response_model=list[Comment], status_code=status.HTTP_200_OK)
+async def get_comments(prompt_id: str, repos: RepositoriesDependency):
+    return await repos.comments.get_from(prompt_id)
+
+
+@router.post("/{prompt_id}/comments", status_code=status.HTTP_201_CREATED)
+async def create_comment(
+    prompt_id: str,
+    comment: CommentCreate,
+    user: UserDependency,
+    repos: RepositoriesDependency
+    ):
+    new_comment = comment.model_dump()
+
+    try:
+        await repos.comments.create(new_comment, user.id, prompt_id)
+    except Exception:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Data base service unavailable",
+        )
+
+    return { "message": "New comment created"}
+
+
+@router.delete("/comments/{comment_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_comment(comment_id: str, user: UserDependency, repos: RepositoriesDependency):
+    comment = await repos.comments.get_by_id(comment_id)
+
+    if user.id != comment.user_id:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="No authorized to delete")
+    
+    await repos.comments.delete(comment_id)
