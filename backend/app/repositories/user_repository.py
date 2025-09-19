506688b019268b3f4b11a3cc0c9b1f5b84d63d8d
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2025-09-19 13:40:59 -0500

    Reduce complexity of user repository

--- backend/app/repositories/user_repository.py
+++ backend/app/repositories/user_repository.py
@@ -1,23 +1,21 @@
 from typing import Optional
 from bson.objectid import ObjectId
+from pymongo.collection import Collection
 
 from app.schemas.user_schema import User
 
 class UserRepository:
     def __init__(self, database):
-        self.collection = database["users"]
+        self.collection: Collection = database["users"]
 
-    async def create_user(self, user_data: User) -> str:
+    async def create(self, user_data: User) -> str:
         result = await self.collection.insert_one(user_data)
         return str(result.inserted_id)
-
-    async def get_user(self, user_id: str) -> Optional[User]:
-        return await self.collection.find_one({"_id": ObjectId(user_id)})
     
-    async def get_user_by_email(self, email: str) -> Optional[User]:
-        return await self.collection.find_one({"email": email})
+    async def get_one_by(self, parameter: dict) -> Optional[User]:
+        return await self.collection.find_one(parameter)
 
-    async def update_user(self, user_id: str, update_data: dict) -> bool:
+    async def update(self, user_id: str, update_data: dict) -> bool:
         result = await self.collection.update_one(
             {"_id": ObjectId(user_id)},
             {"$set": update_data},
