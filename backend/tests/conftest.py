e9f46d942c216b11f3fc0fd52f717547a7b27b56
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2025-10-13 17:27:07 -0500

    Add fixtures to generate a service manager object and a test async client

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,15 +1,47 @@
 # conftest.py
+from httpx import ASGITransport, AsyncClient
+
 import pytest
 from testcontainers.mongodb import MongoDbContainer
 from pymongo import AsyncMongoClient
+
+from app.services.services_manager import ServiceManager
+from app.dependencies.database_deps import get_services
+from app.main import app
+
 
 @pytest.fixture(scope="session")
 def mongo_connection_url():
     with MongoDbContainer("mongodb/mongodb-community-server:8.0-ubi8") as mongo:
         yield mongo.get_connection_url()
 
-@pytest.fixture
-async def mongo_client(mongo_connection_url):
+
+@pytest.fixture()
+async def db(mongo_connection_url):
     client = AsyncMongoClient(mongo_connection_url)
-    yield client
-    await client.close()
+    db = client.get_database("test_db")
+    yield db
+    collections = await db.list_collection_names()
+    for coll in collections:
+        await db[coll].delete_many({})
+    client.close()
+
+
+@pytest.fixture()
+def services(db):
+    return ServiceManager(db)
+
+
+@pytest.fixture
+async def e2e_client(db):
+    service_manager = ServiceManager(db)
+    app.state.database = db
+
+    app.dependency_overrides.clear()
+    app.dependency_overrides[get_services] = lambda: service_manager
+
+    # Crear cliente HTTP de prueba
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://test"
+    ) as ac:
+        yield ac
