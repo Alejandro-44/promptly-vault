bdba81bcb96a4a952545b620114cf11a801b321c
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2025-10-19 22:57:57 -0500

    Modify auth dependency to look for cookie before check auth headers

--- backend/app/dependencies/auth_deps.py
+++ backend/app/dependencies/auth_deps.py
@@ -3,6 +3,7 @@ from typing import Annotated
 from fastapi import HTTPException, Request, status
 from fastapi.params import Depends
 from fastapi.security import OAuth2PasswordBearer
+from jwt import ExpiredSignatureError, PyJWTError
 
 from app.dependencies import ServicesDependency
 from app.core.security import decode_access_token
@@ -16,19 +17,35 @@ OAuth2Dependency = Annotated[str, Depends(oauth2_scheme)]
 async def get_current_user(
     request: Request,
     services: ServicesDependency,
-    token: OAuth2Dependency
+    token: OAuth2Dependency | None = None
 ):
-    if not token:
-        token = request.cookies.get("access_token")
+    cookie_token = request.cookies.get("access_token")
+    
+    token = cookie_token or token
 
     if not token:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Token no encontrado",
+            detail="Token not found",
             headers={"WWW-Authenticate": "Bearer"},
         )
 
-    payload = decode_access_token(token)
+    try:
+        payload = decode_access_token(token)
+    except ExpiredSignatureError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    except PyJWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    
+
     user_id: str = payload.get("sub")
 
     if not user_id:
