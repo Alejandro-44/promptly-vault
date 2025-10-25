e55d6891debd63f115136f1852e710cfda936024
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2025-10-25 18:16:57 -0500

    Add tests for comments service

---
+++ backend/tests/integration/test_comments_service_integration.py
@@ -0,0 +1,70 @@
+import pytest
+from bson import ObjectId
+from app.schemas.comment_schema import CommentCreate, CommentUpdate
+from app.core.exceptions import CommentNotFoundError
+
+MOCK_USER_ID = str(ObjectId())
+MOCK_PROMPT_ID = str(ObjectId())
+MOCK_RANDOM_ID = str(ObjectId())
+
+
+@pytest.mark.asyncio
+async def test_create_comment_success(services):
+    mock_comment = CommentCreate(content="Awesome!")
+    comment_id = await services.comments.create(MOCK_PROMPT_ID, MOCK_USER_ID, mock_comment)
+    assert isinstance(comment_id, str)
+
+
+@pytest.mark.asyncio
+async def test_create_comments_and_get(services):
+    mock_comment_a = CommentCreate(content="Hey! It's useful")
+    mock_comment_b = CommentCreate(content="Awesome!")
+    
+    await services.comments.create(MOCK_PROMPT_ID, MOCK_USER_ID, mock_comment_a)
+    await services.comments.create(MOCK_PROMPT_ID, MOCK_RANDOM_ID, mock_comment_b)
+
+    comments = await services.comments.get_prompt_comments(MOCK_PROMPT_ID)
+
+    assert len(comments) == 2
+
+
+@pytest.mark.asyncio
+async def test_update_comment_success(services):
+    mock_comment = CommentCreate(
+        content="Awesome!"
+    )
+
+    comment_id = await services.comments.create(MOCK_PROMPT_ID, MOCK_USER_ID, mock_comment)
+
+    update_data = CommentUpdate(
+        content="WOW!"
+    )
+
+    result = await services.comments.update(comment_id, MOCK_USER_ID, update_data)
+
+    assert result
+
+
+@pytest.mark.asyncio
+async def test_update_prompt_not_found_raises_error(services):
+    with pytest.raises(CommentNotFoundError):
+        await services.comments.update(
+            MOCK_RANDOM_ID, MOCK_USER_ID, CommentCreate(content="Test")
+        )
+
+
+@pytest.mark.asyncio
+async def test_delete_prompt_success(services):
+    mock_comment = CommentCreate(
+        content="Awesome!"
+    )
+    comment_id = await services.comments.create(MOCK_PROMPT_ID, MOCK_USER_ID, mock_comment)
+
+    comments = await services.comments.get_prompt_comments(MOCK_PROMPT_ID)
+    assert len(comments) == 1
+
+    result = await services.comments.delete(comment_id, MOCK_USER_ID)
+    assert result is True
+
+    comments = await services.comments.get_prompt_comments(MOCK_PROMPT_ID)
+    assert len(comments) == 0
