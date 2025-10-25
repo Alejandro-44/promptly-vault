4e165ca0e5a18c2d325a1f96c12a800ab2f13138
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2025-10-24 23:19:48 -0500

    Fix issue with update  method in comments repository

--- backend/app/repositories/comments_repository.py
+++ backend/app/repositories/comments_repository.py
@@ -14,11 +14,11 @@ class CommentsRepository:
     async def create(self, comment_data: dict):
         return await self.collection.insert_one(comment_data)
     
-    async def update(self, comment_id: str, user_id: str, comment_data: dict):
-        result = await self.collection.update_one({ "_id": ObjectId(comment_id), "user_id": ObjectId(user_id) }, comment_data) 
+    async def update(self, comment_id: str, user_id: str, comment_data: dict) -> bool:
+        result = await self.collection.update_one({ "_id": ObjectId(comment_id), "user_id": ObjectId(user_id) }, { "$set": comment_data }) 
         return result.modified_count > 0
     
-    async def delete(self, comment_id: str, user_id: str):
+    async def delete(self, comment_id: str, user_id: str) -> bool:
         result = await self.collection.delete_one({ "_id": ObjectId(comment_id), "user_id": ObjectId(user_id) })
         return result.deleted_count > 0
  
