27827e88c7660fdbeb54bc5b744a02332dc65263
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2025-10-30 20:43:30 -0500

    simplify config class

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,12 +1,10 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
     PROJECT_NAME: str = "Promptly Vault API"
     MONGO_URI: str = "mongodb://localhost:27017"
     MONGO_DB: str = "promptly"
-
     JWT_SECRET: str = "supersecret" 
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
