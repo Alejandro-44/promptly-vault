3901dcf70c8fc03e1bcd9e32f4e1f8414f7eaf20
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2025-11-07 18:47:19 -0500

    Configure vite for testing and use taildwindcss

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,23 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'node:path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    }
+  },
+  plugins: [
+    tailwindcss(),
+    react(),
+  ],
+  test: {
+    environment: "happy-dom",
+    setupFiles: ["./src/tests/setup.ts"],
+    include: ["src/**/*.test.{ts,tsx}"],
+    globals: true,
+  }
 })
