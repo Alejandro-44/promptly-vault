1466d33bb59c86dd8f7aa6b9ac583833749ec1d6
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2025-11-07 19:03:38 -0500

    Add cors middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pymongo import AsyncMongoClient
 
 from app.routes import auth_routes, user_routes, prompt_routes
@@ -32,6 +33,18 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan, title=settings.PROJECT_NAME)
+
+origins = [
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_routes.router)
 app.include_router(user_routes.router)
