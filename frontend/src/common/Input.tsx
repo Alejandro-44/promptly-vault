4202fe284965244346901c0cb314e449dc57ba27
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2025-11-15 09:58:10 -0500

    Add some styles for register form

--- frontend/src/common/Input.tsx
+++ frontend/src/common/Input.tsx
@@ -12,14 +12,14 @@ function Input({ children, type, name, placeholder }: Props) {
   const { error } = getFieldState(name, formState);
   return (
       <div>
-        <label className="form-label" htmlFor={name}>{children}</label>
+        <label className="text-sm font-medium" htmlFor={name}>{children}</label>
         <input
           id={name}
           aria-label={name}
           placeholder={placeholder}
           type={type}
           {...register(name)}
-          className="border p-2 rounded w-full"
+          className="border border-gray-200 rounded w-full h-9 outline-0 p-6 text-sm placeholder:text-sm placeholder:font-medium"
         />
         {error?.message && (
           <p className="text-red-600 text-sm mt-1">
--- frontend/src/features/auth/pages/RegisterPage.tsx
+++ frontend/src/features/auth/pages/RegisterPage.tsx
@@ -22,31 +22,38 @@ export function RegisterPage() {
 
   return (
     <FormProvider {...methods}>
-      <div className="max-w-md mx-auto p-4 border rounded-lg">
-        <h1 className="text-xl mb-4 font-semibold">Registro</h1>
+      <div className="flex align-middle justify-center h-dvh">
+        <div className="w-full max-w-md shadow-lg rounded-2xl border border-gray-200 p-6 m-auto">
+          <h1 className="text-xl mb-4 font-semibold text-center">Register</h1>
 
-        <form onSubmit={methods.handleSubmit(onSubmit)} className="flex flex-col gap-3">
-          <Input type="text" name="username" placeholder="Username">
-            Username
-          </Input>
-          <Input type="email" name="email" placeholder="Email">
-            Email
-          </Input>
-          <Input type="password" name="password" placeholder="Password">
-            Password
-          </Input>
-
-          <button
-            type="submit"
-            disabled={isPending}
-            className="bg-blue-600 text-white p-2 rounded"
+          <form
+            onSubmit={methods.handleSubmit(onSubmit)}
+            className="flex flex-col space-y-5"
           >
-            {isPending ? "Loading..." : "Register"}
-          </button>
-        </form>
+            <Input type="text" name="username" placeholder="Username">
+              Username
+            </Input>
+            <Input type="email" name="email" placeholder="Email">
+              Email
+            </Input>
+            <Input type="password" name="password" placeholder="••••••••">
+              Password
+            </Input>
 
-        {isSuccess && <p className="text-green-600 mt-4">Successful registration</p>}
-        {error && <p className="text-red-600 mt-4">{errorMessage}</p>}
+            <button
+              type="submit"
+              disabled={isPending}
+              className={"bg-black hover:bg-gray-800 text-indigo-50 rounded-2xl h-10 px-4 py-2 cursor-pointer"}
+            >
+              {isPending ? "Loading..." : "Register"}
+            </button>
+          </form>
+
+          {isSuccess && (
+            <p className="text-green-600 mt-4">Successful registration</p>
+          )}
+          {error && <p className="text-red-600 mt-4">{errorMessage}</p>}
+        </div>
       </div>
     </FormProvider>
   );
