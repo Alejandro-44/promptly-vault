f7cd5b7c966ce4a10d18fdfd8777a996fd24eab8
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2025-11-17 18:23:06 -0500

    add handler for login endpoint

--- frontend/src/tests/mocks/handlers/authHandler.ts
+++ frontend/src/tests/mocks/handlers/authHandler.ts
@@ -3,8 +3,8 @@ import { delay, http, HttpResponse } from "msw";
 
 export const authHandlers = [
   http.post("http://localhost:8000/auth/register", async ({ request }) => {
-    await delay(150)
-    const body = await request.json() as User;
+    await delay(150);
+    const body = (await request.json()) as User;
     if (body.email !== "fail@example.com") {
       return HttpResponse.json(
         {
@@ -16,6 +16,26 @@ export const authHandlers = [
         { status: 201 }
       );
     }
-    return HttpResponse.json({ detail: "Email already registered" }, { status: 409 });
+    return HttpResponse.json(
+      { detail: "Email already registered" },
+      { status: 409 }
+    );
+  }),
+  http.post("http://localhost:8000/auth/login", async ({ request }) => {
+    await delay(150);
+    const body = (await request.json()) as User;
+    if (body.email !== "fail@example.com") {
+      return HttpResponse.json(
+        {
+          token_type: "bearer",
+          access_token: "mockedtoken",
+        },
+        { status: 200 }
+      );
+    }
+    return HttpResponse.json(
+      { detail: "Invalid credentials" },
+      { status: 401 }
+    );
   }),
 ];
