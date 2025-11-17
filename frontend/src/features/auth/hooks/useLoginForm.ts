f7ea8adaf77b99568013ae42f7148b071c240e79
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2025-11-17 17:22:44 -0500

    add login form hook

---
+++ frontend/src/features/auth/hooks/useLoginForm.ts
@@ -0,0 +1,10 @@
+import { useForm } from "react-hook-form";
+import { loginSchema, type LoginFormValues } from "../schemas/login.schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+export function useLoginForm() {
+  return useForm<LoginFormValues>({
+    resolver: zodResolver(loginSchema),
+    criteriaMode: "all",
+  });
+}
