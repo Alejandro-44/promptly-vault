ae2c9d3e5d7121fd44499d40f4df24d3e59f7254
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2025-11-26 09:08:09 -0500

    add tests for login and register functions

--- frontend/src/tests/mocks/handlers/usersHandler.ts
+++ frontend/src/tests/mocks/handlers/usersHandler.ts
@@ -1,4 +1,5 @@
 import { http, HttpResponse } from "msw";
+import { prompts } from "../data/mocks";
 
 export const userHandlers = [
   http.get("http://localhost:8000/users/me", async () => {
@@ -7,5 +8,8 @@ export const userHandlers = [
       email: "johndoe@example.com",
       is_active: true,
     })
+  }),
+  http.get("http://localhost:8000/users/me/prompts", async () => {
+    return HttpResponse.json(prompts)
   })
 ];
