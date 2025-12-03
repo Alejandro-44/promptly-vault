50bd3b73bfbdccae1ab867d35d05d620c4513959
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2025-12-03 11:17:34 -0500

    Add layout components from mui

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -7,8 +7,7 @@ export function Layout() {
         <Link to="/">Inicio</Link>
         <Link to="/register">Registro</Link>
       </nav>
-
-      <main className="flex-1 p-8">
+      <main>
         <Outlet />
       </main>
     </div>
