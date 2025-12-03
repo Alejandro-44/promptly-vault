8b0a8c12ba2231decc3c1d95243cd005448d282a
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2025-12-03 12:12:22 -0500

    add mock user

--- frontend/src/tests/mocks/data/mocks.ts
+++ frontend/src/tests/mocks/data/mocks.ts
@@ -1,23 +1,31 @@
 export const prompts = [
   {
-    "title": "Integration test",
-    "prompt": "Write a poem",
-    "result_example": "A small poem",
-    "model": "gpt-5",
-    "tags": ["ai", "poem"]
+    title: "Integration test",
+    prompt: "Write a poem",
+    result_example: "A small poem",
+    model: "gpt-5",
+    tags: ["ai", "poem"],
   },
   {
-    "title": "Write a perfect essay",
-    "prompt": "Write a essay...",
-    "result_example": "A prefect essay...",
-    "model": "gpt-5",
-    "tags": ["ai", "poem"]
+    title: "Write a perfect essay",
+    prompt: "Write a essay...",
+    result_example: "A prefect essay...",
+    model: "gpt-5",
+    tags: ["ai", "poem"],
   },
   {
-    "title": "Integration test",
-    "prompt": "Write a poem",
-    "result_example": "A small poem",
-    "model": "Claude",
-    "tags": ["ai", "poem"]
-  }
-]
+    title: "Integration test",
+    prompt: "Write a poem",
+    result_example: "A small poem",
+    model: "Claude",
+    tags: ["ai", "poem"],
+  },
+];
+
+export const users = [
+  {
+    id: 1,
+    username: "testuser",
+    email: "exmapleuser@mock.com",
+  },
+];
