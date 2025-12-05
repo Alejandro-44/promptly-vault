1f94f29825f749bf8178516b12257002bb2a99d6
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2025-12-04 22:10:16 -0500

    fix user card test with new parameters

--- frontend/src/features/users/page/UserPage.tsx
+++ frontend/src/features/users/page/UserPage.tsx
@@ -2,10 +2,9 @@ import { Container } from "@mui/material";
 import UserCard from "../components/UserCard";
 
 export default function UserPage() {
-
   return (
-    <Container sx={{ mt: 4}}>
+    <Container sx={{ mt: 4 }}>
       <UserCard />
     </Container>
-  )
+  );
 }
