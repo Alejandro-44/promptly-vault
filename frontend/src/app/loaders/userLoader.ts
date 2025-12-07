5ba51e648cdccb41ed0427ce18285b481e628501
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2025-12-06 22:03:07 -0500

    Fixing loader

--- frontend/src/app/loaders/userLoader.ts
+++ frontend/src/app/loaders/userLoader.ts
@@ -1,9 +1,10 @@
-import { getCurrentUser } from "@/features/users/api/getCurrentUser";
 import { useUserStore } from "@/features/users/contexts";
+import { AuthService } from "@/services";
 
 export async function rootLoader() {
+  console.log("Running rootLoader to fetch user data");
   try {
-    const user = await getCurrentUser();
+    const user = await AuthService.getMe();
     useUserStore.getState().setUser(user);
   } catch (error) {
     useUserStore.getState().clearUser();
