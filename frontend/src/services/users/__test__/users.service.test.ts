6385b14a5c5e1e8ab2681dc6d2de517cd7829f2c
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2025-12-07 18:45:32 -0500

    Add users service and add other user in mocks to test

---
+++ frontend/src/services/users/__test__/users.service.test.ts
@@ -0,0 +1,34 @@
+import { UsersService } from "../users.service";
+
+describe("UsersService", () => {
+  it("should get the current logged user", async () => {
+    const user = await UsersService.getMe();
+
+    expect(user).toEqual({
+      id: "1",
+      username: "johndoe",
+      email: "johndoe@example.com",
+      isActive: true,
+    });
+  });
+
+  it("should delete the current logged user", async () => {
+    await expect(UsersService.deleteMe()).resolves.toBeUndefined();
+  });
+
+  it("should get the current user's prompts", async () => {
+    const prompts = await UsersService.getMyPrompts();
+    expect(prompts).toHaveLength(3);
+  });
+
+  it("should get a user by ID", async () => {
+    const user = await UsersService.getUserById("2");
+
+    expect(user).toEqual({
+      id: "2",
+      username: "janedoe",
+      email: "janedoe@example.com",
+      isActive: true,
+    });
+  });
+});
--- frontend/src/tests/mocks/handlers/usersHandler.ts
+++ frontend/src/tests/mocks/handlers/usersHandler.ts
@@ -7,5 +7,11 @@ export const userHandlers = [
   }),
   http.get("http://127.0.0.1:8000/users/me/prompts", async () => {
     return HttpResponse.json(prompts)
-  })
+  }),
+  http.delete("http://127.0.0.1:8000/users/me", async () => {
+    return HttpResponse.json({}, { status: 204 });
+  }),
+  http.get("http://127.0.0.1:8000/users/2", async () => {
+    return HttpResponse.json(users[1])
+  }),
 ];
