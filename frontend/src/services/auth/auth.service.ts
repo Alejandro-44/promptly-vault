efbaeb92a050f9af0d7567217656e3cfd780e944
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2025-12-06 21:33:09 -0500

    Add auth service, dtos, models and mappers

---
+++ frontend/src/services/auth/auth.service.ts
@@ -0,0 +1,43 @@
+import { httpClient } from "../api/httpClient";
+import type {
+  UserCreateDTO,
+  UserLoginDTO,
+  UpdatePasswordDTO,
+  TokenDTO,
+} from "./auth.dto";
+import type { UserDTO } from "../users/user.dto";
+
+import { authMapper } from "./auth.mapper";
+import { userMapper } from "../users/user.mapper";
+import type { User } from "../users/user.model";
+import type { Token } from "./auth.model";
+
+export class AuthService {
+  static async register(data: UserCreateDTO): Promise<User> {
+    const response = await httpClient.post<UserDTO>("/auth/register", data);
+    return userMapper.toUser(response);
+  }
+
+  static async login(data: UserLoginDTO): Promise<Token> {
+    const response = await httpClient.post<TokenDTO>("/auth/login", data);
+    return authMapper.toToken(response);
+  }
+
+  static async logout(): Promise<void> {
+    await httpClient.post<void>("/auth/logout");
+    localStorage.removeItem("token");
+  }
+
+  static async changePassword(data: UpdatePasswordDTO): Promise<void> {
+    await httpClient.post<void>("/auth/change-password", data);
+  }
+
+  static async getMe(): Promise<User> {
+    const response = await httpClient.get<UserDTO>("/users/me");
+    return userMapper.toUser(response);
+  }
+
+  static async deleteMe(): Promise<void> {
+    await httpClient.delete<void>("/users/me");
+  }
+}
