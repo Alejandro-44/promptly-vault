2b54f511946f078996f7674ff405e5dc4d3d1155
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2025-12-06 21:34:22 -0500

    export all necessary members from services

---
+++ frontend/src/services/index.ts
@@ -0,0 +1,9 @@
+export { AuthService } from "./auth/auth.service";
+export type { User } from "./users/user.model";
+export type { Token } from "./auth/auth.model";
+export type {
+  UserCreateDTO,
+  UserLoginDTO,
+  UpdatePasswordDTO,
+} from "./auth/auth.dto";
+export type { UserDTO } from "./users/user.dto";
