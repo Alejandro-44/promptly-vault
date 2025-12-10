4d5df60f4407bfe12a7a9c6f429b0d445d777587
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2025-12-09 21:54:00 -0500

    Add prompts drid to user page

--- frontend/src/features/users/page/UserPage.tsx
+++ frontend/src/features/users/page/UserPage.tsx
@@ -1,10 +1,13 @@
 import { Container } from "@mui/material";
 import UserCard from "../components/UserCard";
+import UserPrompts from "../components/UserPrompts";
 
 export default function UserPage() {
+
   return (
     <Container sx={{ mt: 4 }}>
       <UserCard />
+      <UserPrompts />
     </Container>
   );
 }
