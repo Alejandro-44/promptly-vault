5206bff8c8ed1ad25bce1592f8a20ca4d5635a05
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2025-12-11 16:26:08 -0500

    update prompt models in prompt services

--- frontend/src/services/prompts/prompts.mapper.ts
+++ frontend/src/services/prompts/prompts.mapper.ts
@@ -1,16 +1,30 @@
-import type { PromptDTO } from "./prompts.dto";
-import type { Prompt } from "./prompts.model";
+import type { PromptDTO, PromptSummaryDTO } from "./prompts.dto";
+import type { Prompt, PromptSummary } from "./prompts.model";
 
 export const promptsMapper = {
-    toPrompt: (dto: PromptDTO): Prompt => ({
+  toPrompt: (dto: PromptDTO): Prompt => ({
     id: dto.id,
     title: dto.title,
     prompt: dto.prompt,
     resultExample: dto.result_example,
     model: dto.model,
     tags: dto.tags,
-    userId: dto.user_id,
-    user: dto.user,
     pubDate: new Date(dto.pub_date),
+    author: {
+      id: dto.author.id,
+      username: dto.author.username,
+      email: dto.author.email,
+    },
   }),
-}
+};
+
+export const promptSummaryMapper = {
+  toPromptSummary: (dto: PromptSummaryDTO): PromptSummary => ({
+    id: dto.id,
+    title: dto.title,
+    tags: dto.tags,
+    model: dto.model,
+    pubDate: new Date(dto.pub_date),
+    authorName: dto.author_name,
+  }),
+};
