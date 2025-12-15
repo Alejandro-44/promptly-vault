c2f91a6fa9ce8e63c1b99d07122cc494ccea6b50
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2025-12-15 11:19:13 -0500

    Remove UserPrompts component and implement useUserPrompts hook in UserPage

--- frontend/src/features/users/hooks/useUserPrompts.ts
+++ frontend/src/features/users/hooks/useUserPrompts.ts
@@ -1,7 +1,7 @@
 import { UsersService } from "@/services";
 import { useQuery } from "@tanstack/react-query";
 
-export function useMyPrompts() {
+export function useUserPrompts() {
   const { data: prompts } = useQuery({
     queryKey: ["myPrompts"],
     queryFn: UsersService.getMyPrompts,
