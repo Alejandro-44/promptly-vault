3900367757540eebf443a16f8e965e5ee6ad4f44
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2025-12-15 09:41:37 -0500

    Refactor exports in index.ts to streamline service and model exports

--- frontend/src/services/index.ts
+++ frontend/src/services/index.ts
@@ -1,11 +1,8 @@
 export { AuthService } from "./auth/auth.service";
 export { UsersService } from "./users/users.service";
-export type { User } from "./users/users.model";
-export type { Token, UserCreate, UserLogin } from "./auth/auth.model";
-export type {
-  UserCreateDTO,
-  UserLoginDTO,
-  UpdatePasswordDTO,
-} from "./auth/auth.dto";
-export type { UserDTO } from "./users/users.dto";
-export type { Prompt } from "./prompts/prompts.model";
+export { PromptsService } from "./prompts/prompts.service";
+export type * from "./users/users.model";
+export type * from "./auth/auth.model";
+export type * from "./auth/auth.dto";
+export type * from "./users/users.dto";
+export type * from "./prompts/prompts.model";
