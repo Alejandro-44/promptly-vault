52ab5d6f88aa25b64caae41786b1fb149e2167c8
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2025-12-16 11:10:44 -0500

    Remove Container wrapper from PromptCardDetail in PromptDetail component

--- frontend/src/features/prompts/pages/PromptDetail.tsx
+++ frontend/src/features/prompts/pages/PromptDetail.tsx
@@ -1,15 +1,12 @@
 import { useParams } from "react-router"
 import { PromptCardDetail } from "../components/PromptCardDetail";
-import { Container } from "@mui/material";
 
 export function PromptDetail() {
   const params = useParams<{ promptId: string }>();
 
   return (
     <>
-      <Container maxWidth="md" sx={{ mt: 4, mb: 4 }}>
-        <PromptCardDetail promptId={params.promptId!} />
-      </Container>
+      <PromptCardDetail promptId={params.promptId!} />
     </>
   )
 }
