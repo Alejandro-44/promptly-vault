995df624dfcd978b6bbcd4d5a481de33aad2f8c4
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2025-12-18 16:37:04 -0500

    Add index file and export member in prompts hook directory

---
+++ frontend/src/features/prompts/hooks/index.ts
@@ -0,0 +1,4 @@
+export { usePrompt } from "./usePrompt";
+export { useCreatePrompt } from "./usePromptCreate";
+export { usePromptForm } from "./usePromptForm";
+export { usePrompts } from "./usePrompts";
