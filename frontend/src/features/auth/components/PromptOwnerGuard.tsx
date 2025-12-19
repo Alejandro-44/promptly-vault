85be61615cbd88cf18deca5fd9e8806fe28471c8
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2025-12-19 16:21:21 -0500

    Add guard to check if current user is the author of the prompt

---
+++ frontend/src/features/auth/components/PromptOwnerGuard.tsx
@@ -0,0 +1,34 @@
+import { Outlet, useNavigate, useParams } from "react-router";
+import { useEffect } from "react";
+import { useAuth } from "../hooks";
+import { usePrompt } from "@/features/prompts/hooks/usePrompt";
+
+export function PromptOwnerGuard() {
+  const { promptId } = useParams();
+  const id = promptId || "";
+  const navigate = useNavigate();
+  const { user } = useAuth();
+  const { data: prompt, isLoading, error } = usePrompt({ promptId: id });
+
+  useEffect(() => {
+    if (error) {
+      navigate("/");
+    }
+  }, [error, navigate]);
+
+  useEffect(() => {
+    if (prompt && prompt.author.id !== user!.id) {
+      navigate("/");
+    }
+  }, [prompt, user, navigate]);
+
+  if (isLoading) {
+    return <div>Cargando prompt...</div>;
+  }
+
+  if (!prompt) {
+    return null;
+  }
+
+  return <Outlet context={{ prompt }} />;
+}
