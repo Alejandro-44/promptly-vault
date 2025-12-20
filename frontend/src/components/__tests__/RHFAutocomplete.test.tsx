d5b24394ac443cc504fc19b34615d21d1db90a6d
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2025-12-20 16:12:47 -0500

    replacing fireEvent with userEvent

--- frontend/src/components/__tests__/RHFAutocomplete.test.tsx
+++ frontend/src/components/__tests__/RHFAutocomplete.test.tsx
@@ -1,10 +1,60 @@
 import { renderWithForm } from "@/tests/utils/renderWithForm";
 import { RHFAutocomplete } from "../RHFAutocomplete";
-import { cleanup, fireEvent, screen } from "@testing-library/react";
+import { cleanup, screen } from "@testing-library/react";
 import { FormValueViewer } from "@/tests/utils/FormValueViewer";
+import userEvent from "@testing-library/user-event";
 
 describe("RHFAutocomplete", () => {
-  beforeEach(() => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  it("renders the autocomplete input", () => {
+    renderWithForm({
+      defaultValues: { tags: [] },
+      children: (
+        <>
+          <RHFAutocomplete
+            name="tags"
+            label="Tags"
+            options={[
+              { id: "react", label: "React" },
+              { id: "ts", label: "TypeScript" },
+            ]}
+          />
+          <FormValueViewer />
+        </>
+      ),
+    });
+    expect(screen.getByLabelText("Tags")).toBeDefined();
+  });
+
+  it("shows options when opened", async () => {
+    renderWithForm({
+      defaultValues: { tags: [] },
+      children: (
+        <>
+          <RHFAutocomplete
+            name="tags"
+            label="Tags"
+            options={[
+              { id: "react", label: "React" },
+              { id: "ts", label: "TypeScript" },
+            ]}
+          />
+          <FormValueViewer />
+        </>
+      ),
+    });
+    const user = userEvent.setup()
+
+    await user.click(screen.getByLabelText("Tags"));
+
+    expect(screen.getByText("React")).toBeDefined();
+    expect(screen.getByText("TypeScript")).toBeDefined();
+  });
+
+  it("updates form value when selecting options", async () => {
     renderWithForm({
       defaultValues: { tags: [] },
       children: (
@@ -22,36 +72,43 @@ describe("RHFAutocomplete", () => {
         </>
       ),
     });
-  });
 
-  afterEach(() => {
-    cleanup();
-  });
+    const user = userEvent.setup()
 
-  it("renders the autocomplete input", () => {
-    expect(screen.getByLabelText("Tags")).toBeDefined();
-  });
-
-  it("shows options when opened", () => {
-    fireEvent.keyDown(screen.getByLabelText("Tags"), { key: "ArrowDown" });
-    expect(screen.getByText("React")).toBeDefined();
-    expect(screen.getByText("TypeScript")).toBeDefined();
-  });
-
-  it("updates form value when selecting options", () => {
-    fireEvent.keyDown(screen.getByLabelText("Tags"), { key: "ArrowDown" });
-    fireEvent.click(screen.getByText("React"));
+    await user.click(screen.getByLabelText("Tags"));
+    await user.click(screen.getByText("React"));
 
     const formValueElement = screen.getByTestId("form-value");
     const text = formValueElement.textContent;
     expect(text).toContain("react");
   });
 
-  it("include multiple values", () => {
-    fireEvent.keyDown(screen.getByLabelText("Tags"), { key: "ArrowDown" });
-    fireEvent.click(screen.getByText("React"));
-    fireEvent.keyDown(screen.getByLabelText("Tags"), { key: "ArrowDown" });
-    fireEvent.click(screen.getByText("TypeScript"));
+  it("include multiple values", async () => {
+    renderWithForm({
+      defaultValues: { tags: [] },
+      children: (
+        <>
+          <RHFAutocomplete
+            multiple
+            name="tags"
+            label="Tags"
+            options={[
+              { id: "react", label: "React" },
+              { id: "ts", label: "TypeScript" },
+            ]}
+          />
+          <FormValueViewer />
+        </>
+      ),
+    });
+
+    const user = userEvent.setup()
+
+    await user.click(screen.getByLabelText("Tags"));
+    await user.click(screen.getByText("React"));
+
+    await user.click(screen.getByLabelText("Tags"));
+    await user.click(screen.getByText("TypeScript"));
 
     const formValueElement = screen.getByTestId("form-value");
     const text = formValueElement.textContent;
