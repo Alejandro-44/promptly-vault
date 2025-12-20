bebbd78daa6c7d2b14a4eeeceabbc37a58553b85
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2025-12-20 14:10:25 -0500

    Change memory router in tests to a router data object

--- frontend/src/tests/utils/renderWithProviders.tsx
+++ frontend/src/tests/utils/renderWithProviders.tsx
@@ -1,9 +1,13 @@
 import { render } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { MemoryRouter, Route, Routes } from "react-router";
+import { createMemoryRouter, RouterProvider } from "react-router";
 import { ProtectedRoute } from "@/features/auth/components/ProtectedRoute";
+import { PromptOwnerGuard } from "@/features/auth/components/PromptOwnerGuard";
 
-export function renderWithProviders(ui: React.ReactNode, initialEntries: string[] = ["/test"]) {
+export function renderWithProviders(
+  ui: React.ReactNode,
+  initialEntries: string[] = ["/test"]
+) {
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
@@ -13,20 +17,65 @@ export function renderWithProviders(ui: React.ReactNode, initialEntries: string[
     },
   });
 
+  const router = createMemoryRouter(
+    [
+      {
+        path: "/",
+        element: <div>Home</div>,
+      },
+      {
+        path: "test",
+        element: ui,
+      },
+      {
+        path: "prompts",
+        children: [
+          {
+            path: ":promptId",
+            children: [
+              {
+                index: true,
+                element: <div>Prompt Detail</div>,
+              },
+              {
+                Component: PromptOwnerGuard,
+                children: [
+                  {
+                    path: "edit",
+                    element: <div>Edit Prompt</div>,
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        path: "users/:id",
+        element: <div>Author page</div>,
+      },
+      {
+        path: "login",
+        element: <div>Login page</div>,
+      },
+      {
+        Component: ProtectedRoute,
+        children: [
+          {
+            path: "private",
+            element: <div>Private Page</div>,
+          },
+        ],
+      },
+    ],
+    {
+      initialEntries,
+    }
+  );
+
   return render(
     <QueryClientProvider client={queryClient}>
-      <MemoryRouter initialEntries={initialEntries}>
-        <Routes>
-          <Route path="/" element={<div>Home</div>} />
-          <Route path="/test" element={ui} />
-          <Route path="/prompts/:id" element={<div>Prompt detail</div>} />
-          <Route path="/users/:id" element={<div>Author page</div>} />
-          <Route path="/login" element={<div>Login page</div>} />
-          <Route element={<ProtectedRoute />}>
-            <Route path="/private" element={<div>Private Page</div>} />
-          </Route>
-        </Routes>
-      </MemoryRouter>
+      <RouterProvider router={router} />
     </QueryClientProvider>
   );
 }
