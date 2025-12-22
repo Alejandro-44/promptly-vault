e0686084a2c793670bbf6c9665e202530a6cf3c9
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2025-12-21 20:29:37 -0500

    add interceptor to get customize message errors

--- frontend/src/services/api/axios.config.ts
+++ frontend/src/services/api/axios.config.ts
@@ -3,10 +3,22 @@ import axios from "axios";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000";
 
-export default axios.create({
+const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
   withCredentials: true,
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.data?.message) {
+      throw new Error(error.response.data.message);
+    }
+    throw error;
+  }
+);
+
+export default api;
