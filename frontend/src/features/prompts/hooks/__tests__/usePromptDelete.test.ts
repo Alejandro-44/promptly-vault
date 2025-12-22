f535866caf234694a514d63f0b8c1bf819df7dbc
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2025-12-21 20:36:17 -0500

    Add tests to new hooks

---
+++ frontend/src/features/prompts/hooks/__tests__/usePromptDelete.test.ts
@@ -0,0 +1,26 @@
+import { renderHookWithClient } from "@/tests/utils/renderHookWithClient";
+import { waitFor } from "@testing-library/react";
+import { usePromptDelete } from "../usePromptDelete";
+
+const navigateMock = vi.fn();
+
+vi.mock("react-router", async () => {
+  const actual = await vi.importActual<any>("react-router");
+  return {
+    ...actual,
+    useNavigate: () => navigateMock,
+  };
+});
+
+
+describe("usePromptDelete", () => {
+  it("redirects to /users/me on success", async () => {
+    const mockPromptId = "abc-123";
+    const { result } = renderHookWithClient(() => usePromptDelete({ promptId: mockPromptId }));
+
+    result.current.mutate(mockPromptId);
+    await waitFor(() => {
+      expect(navigateMock).toHaveBeenCalledWith("/users/me");
+    })  
+  });
+});
---
+++ frontend/src/features/prompts/hooks/__tests__/usePromptUpdate.test.ts
@@ -0,0 +1,26 @@
+import { renderHookWithClient } from "@/tests/utils/renderHookWithClient";
+import { usePromptUpdate } from "../usePromptUpdate";
+import { waitFor } from "@testing-library/react";
+
+const navigateMock = vi.fn();
+
+vi.mock("react-router", async () => {
+  const actual = await vi.importActual<any>("react-router");
+  return {
+    ...actual,
+    useNavigate: () => navigateMock,
+  };
+});
+
+
+describe("usePromptUpdate", () => {
+  it("redirects to /prompts/:promptId on success", async () => {
+    const mockPromptId = "abc-123";
+    const { result } = renderHookWithClient(() => usePromptUpdate({ promptId: mockPromptId }));
+
+    result.current.mutate(mockPromptId);
+    await waitFor(() => {
+      expect(navigateMock).toHaveBeenCalledWith("/prompts/abc-123");
+    })  
+  });
+});
